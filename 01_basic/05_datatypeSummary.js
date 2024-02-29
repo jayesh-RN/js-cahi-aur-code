e8c5d8c3a90b6fab174995528867570ee7ce56d8
Jayesh Pandey <100137975+jayesh-RN@users.noreply.github.com>
2024-02-29 12:02:38 +0000

    comparision

---
+++ 01_basic/05_datatypeSummary.js
@@ -0,0 +1,20 @@
+// primitive call by value 
+// 7 types : String , number , boolean , null, undefined , symbol ,BigInt
+
+
+// javascript is dynammically typed language
+
+
+
+// Non primitive --> refrence type
+// Array,Objects,functions
+
+// Array = []
+// object = {}
+// function(){}
+
+
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
