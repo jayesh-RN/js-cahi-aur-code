31de09c9951cfe300955bf299c5f384c78e2e66f
Jayesh Pandey <100137975+jayesh-RN@users.noreply.github.com>
2024-02-29 10:42:19 +0000

    data type

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3)  we are using nodejs not browser
+
+console.log(3+3)//code readability should be high
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number=> 2 to 2^53
+// big int
+// string => ""
+// boolean =>true/false
+// null =? standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof null);
+console.log(typeof undefined);
